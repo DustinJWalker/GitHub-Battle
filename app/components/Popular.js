b173bc1e4b595122dac648c9c1dcd7145ebc15d4
Dustin Walker <dwtimp@gmail.com>
2017-05-11 15:08:28 -0500

    adding state

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -24,12 +24,31 @@ function RepoGrid (props) {
   return (
     <ul class="popular-list">
         {props.repos.map(function (repo, index) {
+          return (
           <li key={repo.name} className="popular-item">
-
+            <div className="popular-rank">#{index + 1}</div>
+            <ul className="space-list-items">
+              <li>
+                <img className="avatar",
+                  src={repo.owner.avatar_url}
+                  alt={'Avatar for ' + repo.owner.login}
+                />
+              </li>
+              <li>
+                  <a href={repo.html_url}>{repo.name}</a>
+              </li>
+              <li>@{repo.owner.login}</li>
+              <li>{repo.stargazers_count} stars</li>
+            </ul>
           </li>
+        )
         })}
-    </ul>
+      </ul>
   )
+}
+
+RepoGrid.propTypes = {
+  repos: PropTypes.array.isRequired,
 }
 
 SelectLanguage.propTypes = {
