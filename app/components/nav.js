875c78b3e9a5f0fe4869a57133aa8bc195652432
Dustin Walker <dwtimp@gmail.com>
2017-05-14 13:01:37 -0500

    started nav links

--- app/components/nav.js
+++ app/components/nav.js
@@ -1,5 +1,14 @@
 var React = require('react');
-
+var Link = require('react-router-dom').Link;
+var NavLink = require('react-router-dom').NavLink;
 function Nav () {
+  return (
+    <ul className='nav'>
+        <li>
 
+        </li>
+    </ul>
+  )
 }
+
+module.exports = Nav;
