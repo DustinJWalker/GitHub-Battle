cc99d9225b9a0b1a380d8fc62246a3294f226f45
Dustin Walker <dwtimp@gmail.com>
2017-05-14 13:05:13 -0500

    added content to NavLink

--- app/components/nav.js
+++ app/components/nav.js
@@ -1,11 +1,25 @@
 var React = require('react');
 var Link = require('react-router-dom').Link;
 var NavLink = require('react-router-dom').NavLink;
+
+
 function Nav () {
   return (
     <ul className='nav'>
         <li>
-
+          <NavLink activeClassName='active' to='/'>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName='active' to='/battle'>
+            battle
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName='active' to='/popular'>
+            Popular
+          </NavLink>
         </li>
     </ul>
   )
