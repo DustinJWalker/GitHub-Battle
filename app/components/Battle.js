35e303e9fad7df50eea8f83a48776d18d723c584
Dustin Walker <dwtimp@gmail.com>
2017-05-21 19:23:24 -0500

    beginning constructor on battle js

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,10 +1,20 @@
 var React = require('react');
 
 class Battle extends React.Copmonent {
+  constructor() {
+    super(props);
+
+    this.state = {
+      playerOneName: '',
+      playerTwoName: '',
+      playerOneImage: null,
+      playerTwoImage: null
+    }
+  }
   render() {
     return (
       <div>
-        Battle
+        Battle!
       </div>
     )
   }
