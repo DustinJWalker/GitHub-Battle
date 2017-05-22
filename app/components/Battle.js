bf9ab2a46e26d0d4f63f82a07a0c92a8c65fdf12
Dustin Walker <dwtimp@gmail.com>
2017-05-21 19:54:32 -0500

    adding css to battle component

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,4 +1,63 @@
 var React = require('react');
+var PropTypes = require('prop-types');
+
+class PlayerInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event)  {
+    var value = event.target.value;
+    this.setState(function () {
+      return {
+        username: value
+      }
+    })
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+
+    this.props.onSubmit(
+      this.props.id,
+      this.state.username
+    )
+  }
+  render () {
+    return (
+      <form className='colum' onSubmit={this.handleSubmit}>
+        <label className='header' htmlFor='username'>
+          {this.props.label}
+        </label>
+        <input
+          id='username'
+          placeholder='github username'
+          type='text'
+          autoComplete='off'
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <button
+          className='button'
+          type='submit'
+          disabled={!this.state.username}>
+          Submit
+        </button>
+      </form>
+    )
+  }
+}
+
+PlayerInput.propType = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired
+}
 
 class Battle extends React.Copmonent {
   constructor() {
@@ -37,7 +96,11 @@ class Battle extends React.Copmonent {
           />}
 
             {!playerTwoName &&
-            <PlayerInput />}
+            <PlayerInput
+              id='playerTwo'
+              label= 'Player Two'
+              onSubmit={this.handleSubmit}
+            />}
         </div>
       </div>
     )
