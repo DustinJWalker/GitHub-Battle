f449c0c281d08f9518dec348b84c1198db785f1c
Dustin Walker <dwtimp@gmail.com>
2017-05-30 19:10:11 -0500

    building ui for battle js Component

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,6 +1,28 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 
+
+function PlayerPreview(props) {
+  return (
+    <div>
+      <div clasName='column'>
+        <img
+          className='avatar'
+          src={props.avatar}
+          alt={'Avatar for ' props.username}
+        />
+        <h2 className="username">@{props.username}</h2>
+      </div>
+
+    </div>
+  )
+}
+
+PlayerPreview.PropTypes = {
+  avatar: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  onReset: PropTypes.function.isRequired
+}
 class PlayerInput extends React.Component {
   constructor(props) {
     super(props);
