976dd4e51abda5539f2e5964f5bbb093df98b3d8
Dustin Walker <dwtimp@gmail.com>
2017-06-11 13:15:10 -0500

    adding get request

--- app/components/api.js
+++ app/components/api.js
@@ -6,6 +6,32 @@ var params= "?clent_id=" +id+ "&client_secret" + sec;
 
 function getProfile (username) {
   return axios.get('https://api.github.com/users/' + username + params)
+    .then(function (user) {
+      return user.data;
+    });
+}
+
+function getRepos (username) {
+  return axios.get('https://api.github.com/users/' +username + '/repos' + params +
+  '&per_page=100')
+}
+
+function getStarCount (repos) {
+  return repos.data.reduce(function(count, repo) {
+    return count + repo.stargazers_count;
+  }, 0)
+}
+
+function calculateScore (profile, repos) {
+  var followers = profile.followers;
+  var totalStars = getStarCount(repos);
+
+  return(followers * 3) + totalStars;
+}
+
+function handleError (error) {
+  console.warn(error);
+  return null;
 }
 module.exports = {
   battle: functions (players) {
