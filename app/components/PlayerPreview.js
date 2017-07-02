963f469e5d913bf5b61ce51a793b21f94fecf1e3
Dustin Walker <dwtimp@gmail.com>
2017-07-02 14:51:01 -0500

    created PlayerPreview.js

---
+++ app/components/PlayerPreview.js
@@ -0,0 +1,22 @@
+var React = require('react');
+var PropTypes = require('prop-types');
+var Link = require('react-router-dom').Link;
+
+function PlayerPreview(props) {
+  return (
+    <div>
+      <div clasName='column'>
+        <img
+          className='avatar'
+          src={props.avatar}
+          alt={'Avatar for ' + props.username}
+        />
+        <h2 className="username">@{props.username}</h2>
+      </div>
+
+      PlayerPreview.PropTypes = {
+        avatar: PropTypes.string.isRequired,
+        username: PropTypes.string.isRequired,
+        id: PropTypes.string.isRequired,
+        onReset: PropTypes.function.isRequired
+      }
