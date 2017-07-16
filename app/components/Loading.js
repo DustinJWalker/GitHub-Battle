5f4376c2595762cd5228337a939bedadc11d73cd
Dustin Walker <dwtimp@gmail.com>
2017-07-16 14:09:08 -0500

    adding JS to loading js

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -1,12 +1,30 @@
 var React = require('react');
 var PropTypes = require('prop-types');
-
+var styles = {
+  content: {
+    textAlign: 'center',
+    fontSize: '35px'
+  }
+}
 class Loading extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state= {
+      text: 'Loading'
+    };
+  }
   render() {
     return (
-        <p>
+        <p style={styles.content}>
           {this.state.text}
         </p>
     )
   }
+}
+Loading.propTypes = {
+  text: PropTypes.string.isRequired
+}
+Loading.defaultProps = {
+  text: 'Loading'
 }
