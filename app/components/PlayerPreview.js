ffca722f177568a6c508ee00f76ad20b9bba0892
Dustin Walker <dwtimp@gmail.com>
2017-07-18 20:59:51 -0500

    requiring loading component to popular component

--- app/components/PlayerPreview.js
+++ app/components/PlayerPreview.js
@@ -1,23 +1,25 @@
 var React = require('react');
 var PropTypes = require('prop-types');
-var Link = require('react-router-dom').Link;
 
-function PlayerPreview(props) {
+function PlayerPreview (props) {
   return (
     <div>
-      <div clasName='column'>
+      <div className='column'>
         <img
           className='avatar'
           src={props.avatar}
           alt={'Avatar for ' + props.username}
         />
-        <h2 className="username">@{props.username}</h2>
+        <h2 className='username'>@{props.username}</h2>
       </div>
       {props.children}
     </div>
-)
+  )
 }
-      PlayerPreview.PropTypes = {
-        avatar: PropTypes.string.isRequired,
-        username: PropTypes.string.isRequired,
-      }
+
+PlayerPreview.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+};
+
+module.exports = PlayerPreview;
